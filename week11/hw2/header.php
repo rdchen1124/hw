79b401177f0b72a44fe15d23160e58460d603d27
rdchen1124 <rdchen1124@gmail.com>
2021-05-13 11:42:33 +0800

    'week11' 之 '將 Navbar 獨立成 header 並在需要顯示的頁面 include' 待完成 '新增文章 功能'

---
+++ week11/hw2/header.php
@@ -0,0 +1,23 @@
+<nav class='navbar'>
+    <div class='navbar__wrapper wrapper'>
+        <div class="navbar__site-name">
+            <a href='index.php'>Ryan's Blog</a>
+        </div>
+        <ul class='navbar__list'>
+            <div>
+                <li><a href="#">分類專區</a></li>
+                <li><a href="#">文章列表</a></li>
+                <li><a href="#">關於我</a></li>
+            </div>
+            <div>
+                <?php if(!$username) {?>
+                    <li><a href="login.php">登入</a></li>
+                <?php }else{ ?>
+                    <li><a href="add_article.php">發布文章</a></li>
+                    <li><a href="admin.php">管理後台</a></li>
+                    <li><a href="logout.php">登出</a></li>
+                <?php } ?>
+            </div>    
+        </ul>
+    </div>
+</nav>
